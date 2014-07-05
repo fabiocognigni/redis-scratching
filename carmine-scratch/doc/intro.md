b2c530ac330c02666b9a7d4442936c556dae0704
Fabio <fabio_cognigni@s5a.com>
2014-07-05 11:50:18 -0400

    first commit: Redis basic commands with Carmine.

---
+++ carmine-scratch/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to carmine-scratch
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ carmine-scratch/test/carmine_scratch/core_test.clj
@@ -0,0 +1,7 @@
+(ns carmine-scratch.core-test
+  (:use clojure.test
+        carmine-scratch.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

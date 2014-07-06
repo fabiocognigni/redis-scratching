0937d3dc7758d4b39e4a325514c8a574b2dde1e8
Fabio <fabio_cognigni@s5a.com>
2014-07-06 10:08:15 -0400

    Added get and set functions

--- carmine-scratch/src/carmine_scratch/core.clj
+++ carmine-scratch/src/carmine_scratch/core.clj
@@ -1,7 +1,8 @@
 (ns carmine-scratch.core
   (:require [taoensso.carmine :as car :refer (wcar)]))
 
-(def server1-conn {:pool {} :spec {}}) ; See `wcar` docstring for opts
+(def server1-conn {:pool {} :spec {}})
+;; See `wcar` docstring for opts
 (defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
 
 (defn foo
@@ -9,3 +10,18 @@
   (wcar* (car/ping)
        (car/set "foo" "bar")
        (car/get "foo")))
+
+(defn set
+  "Sets a key - value entry and the inverted entry for reverse lookup"
+  [key val]
+  (wcar* (car/set key val)
+         (car/set val key)))
+
+(defn get
+  "Get the value by the key passed as param"
+  [key]
+  (wcar* (car/get key)))
+
+(comment
+  (set "key-1" "val-1")
+  (get "key-1"))
